91e7f994d018dcbf731b8ef2ee043674397079b3
armsmart10 <your-email@example.com>
2025-03-02 09:34:21 -0500

    Fix 'auth' undefined error in Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Use Link for navigation
+import { auth } from '../firebase'; // Import the auth object from Firebase configuration
 import './Navbar.css';
 
 const Navbar = ({ currentUser }) => {
